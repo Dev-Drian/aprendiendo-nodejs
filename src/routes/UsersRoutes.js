2cc974fa9f89dbdadf34ca0b9661ce40f5454e44
Adrian Castro <programador13@tecnologicocomfenalco.edu.co>
2025-05-12 17:19:03 -0500

    Se agregó un middleware de validación de usuario en las rutas de creación y actualización de usuarios, mejorando la seguridad y la integridad de los datos en la aplicación.

--- src/routes/UsersRoutes.js
+++ src/routes/UsersRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const router = express.Router();
 import { getAllUsers, getUserById, createUser, updateUser, deleteUsers } from '../controllers/userController.js'
+import { validateUser } from '../middlewares/validateUser.js';
+
 
 router.get('/', getAllUsers);
 router.get('/:id', getUserById);
-router.post('/', createUser);
-router.put('/:id', updateUser);
+router.post('/', validateUser, createUser);
+router.put('/:id', validateUser, updateUser);
 router.delete('/:id', deleteUsers);
 
 export default router;
